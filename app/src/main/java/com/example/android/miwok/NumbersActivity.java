d7df755130cb46d6db44bef8f4331f7ac66b49ab
annedawson <anne.dawson@gmail.com>
2018-08-06 08:52:19 -0700

    Mon 6 Aug 2018

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +29,36 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // create an array of words (here at Sun 5 Aug 2918, 10:33 PT)
+        // String[] words = new String[10];
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add("one") ;
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        //Log.v("'\n' + NumbersActivity", "Word at index 0: " + words.get(0));
+
+        // Find the root view so we can add child views to it
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        // as long as the current index position is less than the length of the list).
+        // The index variable keeps track of our current position in the list and
+        // increments by 1 each time the code in the loop is executed.
+        for (int index = 0; index < words.size(); index++) {
+            // Create a new TextView
+            TextView wordView = new TextView(this);
+            // Set the text to be word at the current index
+            wordView.setText(words.get(index));
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+            }
     }
 }
